1f77edd7dfdfbbc944ded428605fd58e36277a7d
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-06-23 12:53:28 -0300

    Soma de inteiros C#

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Course
 {
@@ -8,27 +7,16 @@ namespace Course
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Digite o seu nome competo");
-            string nome = Console.ReadLine();
+            Console.WriteLine("Digite um número");
+            int numero1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Quantos quartos tem na sua casa?");
-            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número");
+            int numero2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Entre com o preço de um produto");
-            double precoProduto = double.Parse(Console.ReadLine());
+            int soma = numero1 + numero2;
 
-            Console.WriteLine("Entre com seu útimo nome, idade e altura na mesma linha");
-            string[] vet = Console.ReadLine().Split(' ');
-            string ultimoNome = vet[0];
-            int idade = int.Parse(vet[1]);
-            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            Console.WriteLine("Soma: "+ soma);
 
-            Console.WriteLine(nome);
-            Console.WriteLine(quartos);
-            Console.WriteLine(precoProduto.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine(ultimoNome);
-            Console.WriteLine(idade);
-            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
