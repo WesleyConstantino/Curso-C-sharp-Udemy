920b8e2b30435f267a4419043b2a33598e405439
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-07-20 14:07:01 -0300

    Individual.cs

---
+++ Herança e polimorfismo/Pessoa física e jurídica/Entities/Individual.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa_física_e_jurídica.Entities
+{
+    class Individual : TaxPlayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual()
+        {
+        }
+
+        public Individual(double healthExpenditures, string name, double anualIncome) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.0)
+            {
+                return AnualIncome * 0.15 - HealthExpenditures * 0.5;
+            }
+            else
+            {
+                return AnualIncome * 0.25 - HealthExpenditures * 0.5;
+            }
+        }
+    }
+}
