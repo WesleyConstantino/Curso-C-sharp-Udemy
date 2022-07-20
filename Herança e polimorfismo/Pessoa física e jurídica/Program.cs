a1a9b0c090ae61f3a3ffed4096ef15bc0db58ea6
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-07-20 14:04:32 -0300

    Program.cs

---
+++ Herança e polimorfismo/Pessoa física e jurídica/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Pessoa_física_e_jurídica.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TaxPlayer> list = new List<TaxPlayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (type == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    
+                    list.Add(new Individual(healthExpenditures, name, anualIncome));
+                }
+
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(numberOfEmployees, name, anualIncome));
+                }
+            }
+
+            double sum = 0.0;
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+            foreach (TaxPlayer tp in list)
+            {
+                double tax = tp.Tax();
+                Console.WriteLine(tp.Name + ": $ " + tax.ToString("F2", CultureInfo.InvariantCulture));
+                sum += tax;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
